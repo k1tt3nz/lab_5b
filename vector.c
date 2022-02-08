48c0ab72752a8afddbd5a1a567f1103ed4d6fd67
k1tt3nz <k1tt3nzpink@gmail.com>
2022-02-09 02:28:25 +0300

    memory usage of vector

--- vector.c
+++ vector.c
@@ -1,5 +1,48 @@
 #include <stdlib.h>
-
 #include "vector.h"
 
+vector createVector(const size_t capacity) {
+    int *data;
+    if (capacity) {
+        data = (int *) malloc(sizeof(int) * capacity);
+        if (data == NULL) {
+            fprintf(stderr, "memory allocation error: system cannot allocate %zu bytes of memory",
+                    capacity * sizeof(int));
+            exit(1);
+        }
+    } else {
+        data = NULL;
+    }
 
+    return (vector) {data, 0, capacity};
+}
+
+void deleteVector(vector *v) {
+    free(v->data);
+    v->data = NULL;
+}
+
+void reserveVector(vector *v, const size_t newCapacity) {
+    if (newCapacity) {
+        v->data = (int *) realloc(v->data, newCapacity * sizeof(int));
+        if (v->data == NULL) {
+            fprintf(stderr, "memory allocation error: system cannot allocate %zu bytes of memory",
+                    newCapacity * sizeof(int));
+            exit(1);
+        }
+    } else {
+        v->data = NULL;
+    }
+    v->capacity = newCapacity;
+    if (v->size > v->capacity) {
+        v->size = v->capacity;
+    }
+}
+
+void clearVector(vector *v) {
+    v->size = 0;
+}
+
+void shrinkToFit(vector *v) {
+    v->capacity = v->size;
+}
--- vector.h
+++ vector.h
@@ -1,4 +1,28 @@
 #ifndef COURSE_VECTOR_H
 #define COURSE_VECTOR_H
 
+#include <stdbool.h>
+#include <stdio.h>
+
+typedef struct vector{
+    int *data;          //Указатель на элементы вектора
+    size_t size;        //Размер вектора
+    size_t capacity;    //Вместимость вектора
+}vector;
+
+//Возвращает структуру-дескриптор вектор из capacity значений
+vector createVector(const size_t capacity);
+
+//Освобождает память выделенную вектору
+void deleteVector(vector *v);
+
+//Изменяет кол-во памяти вместимости вектора на значение newCapacity
+void reserveVector(vector *v, const size_t newCapacity);
+
+//Удаляет элементы из контейнера, но не освобождает память
+void clearVector(vector *v);
+
+//Освобождает память выделенную под неиспользуемые элементы
+void shrinkToFit(vector *v);
+
 #endif //VECTOR_H
