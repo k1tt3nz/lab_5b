2aaf23d2171873677b8f3c7c45f90776b6a000ae
k1tt3nz <k1tt3nzpink@gmail.com>
2022-02-09 02:22:33 +0300

    First commit

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "test_ds_vector.h"
+
+int main() {
+    test_createVector_deleteVector();
+
+    return 0;
+}
---
+++ test_ds_vector.c
@@ -0,0 +1,5 @@
+#include "test_ds_vector.h"
+#include "vector.h"
+
+#include <assert.h>
+
